271a91a89600b1df9e101caa55251fd9da198b6f
Daniel Ruiz <danruicar@gmail.com>
2017-04-22 14:35:08 +0200

    Added test for simple multiplication

--- MoneyTalks/Euro.swift
+++ MoneyTalks/Euro.swift
@@ -3,4 +3,13 @@ import Foundation
 
 struct Euro {
     
+    var _amount: Int
+    
+    init(_ amount: Int = 0) {
+        _amount = amount
+    }
+    
+    func times(_ factor: Int) -> Euro {
+        return Euro(_amount * factor)
+    }
 }
--- MoneyTalksTests/EuroTests.swift
+++ MoneyTalksTests/EuroTests.swift
@@ -4,8 +4,21 @@ import XCTest
 
 class EuroTests: XCTestCase {
     
+    var five: Euro!
+    
+    override func setUp() {
+        super.setUp()
+        
+        five = Euro(5)
+    }
+    
     func testCanCreateEuro() {
         XCTAssertNotNil(Euro())
+    }
+    
+    func  testSimpleMultiplication(){
+        let ten = five.times(2)
+        XCTAssertEqual(ten._amount, 10)
     }
     
 }
